bc56c37f7ecfac64ded019cdb0f63c3e9e13b6c1
fusie <stud.fusie@gdromagnosi.edu.it>
2026-03-27 12:37:13 +0000

    ftfhtf

---
+++ esercizioduearray.cpp
@@ -0,0 +1,34 @@
+// Crea due array di 4 elementi ciascuno. Somma elemento per elemento e stampa il risultato in un terzo array.
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    int n=4;
+    int x[n];
+    int y[n];
+    int somma[n];
+    
+
+    for(int i=0; i<n; i++){
+        cout<<"inserisci i numeri del primo array"<<endl;
+         x[n];
+    }
+    for(int i=0; i<n; i++){
+        cout<<"inserisci i numeri del secondo array"<<endl;
+         y[n];
+    }
+    
+    for(int i=0; i<n; i++){
+    somma[i]=x[i]+y[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<"la somma dei due array è"<<somma[i]<<endl;
+    }
+         
+
+
+
+}
---
+++ esercizioinseriscinumerienumeriuguali.cpp
@@ -0,0 +1,38 @@
+// Leggi numeri finché l’utente inserisce 0. Verifica se tutti i numeri inseriti sono uguali altrimenti stampa diversi
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+/* int i;
+cin>>i;
+cout<<"inserisci il numero"<<endl;
+
+if(i!=0){
+
+    while()
+
+}/*
+
+
+   int i;
+   int x;
+    do{
+
+     cout<<"inserisci un numero"<<endl;
+     cin>>i;
+if(i!=0){
+    
+
+
+     }
+    while(i!=0);
+  if(i==x){
+
+    cout<<x<<endl;
+  }
+    
+}
+}
+
